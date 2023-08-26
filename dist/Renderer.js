1b1a639cb0d113dcb4bd01b62033e2214e61ebbd
vickiMen <vicki.menashe@gmail.com>
2023-08-26 22:44:37 +0300

    renderer + controller in client side

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -5,10 +5,45 @@ class Renderer {
         this.tasksListElement = document.getElementsByClassName('tasks-list')[0];;
     };
 
-    renderData(allTasks) {
-        this._clearTasksList; // empty the list first
-        // show all tasks from the server
+    renderData(tasks) {
+        this.tasksListElement.innerHTML = ''; //empty the list first
+        tasks.forEach(task => {
+            this.addTask(task)
+        });
     };
+
+    addTask(task) {
+        const newTaskElement = this._createTaskElement(task);
+        this.tasksListElement.appendChild(newTaskElement);
+        this.inputElement.value = ''; // clear the input value
+    }
+
+    _createTaskElement(task) {
+        const element = document.createElement('div');
+        element.className = 'task';
+        element.id = task.id;
+        const taskTitle = this._createTaskTitle(task.title);
+        const taskDeleteButton = this._createTaskDeleteButton(task.id);
+        element.append(taskTitle, taskDeleteButton);
+        return element;
+    };
+
+    _createTaskTitle(value) {
+        const element = document.createElement('div');
+        element.className = 'task-title';
+        element.innerHTML = value;
+        return element;
+    }
+
+    _createTaskDeleteButton(id) {
+        const element = document.createElement('button');
+        element.className = 'task-delete';
+        element.id = id;
+        element.innerHTML = 'X';
+        element.onclick = deleteTask;
+        return element;
+    }
+
 };
 
 const renderer = new Renderer();
--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,18 @@ const loadPage = async () => {
     renderer.renderData(controller.tasks);
 }
 
+const addTask = async () => {
+    const inputValue = document.getElementsByTagName('input')[0].value;
+    await controller.addTask(inputValue);
+    renderer.renderData(controller.tasks);
+}
+
+const deleteTask = async (event) => {
+    const taskIdToDelete = event.target.id;
+    await controller.deleteTask(taskIdToDelete);
+    renderer.renderData(controller.tasks);
+}
+
 (async function() {
 	await loadPage();
 })();
