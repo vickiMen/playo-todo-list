be144e62e5071d45f0bd304fbee006dd32041c9c
vickiMen <vicki.menashe@gmail.com>
2023-08-26 23:22:31 +0300

    restored controller.js

--- dist/Controller.js
+++ dist/Controller.js
@@ -2,14 +2,36 @@ class Controller {
 
     constructor() {
         this.tasks = [];
-    };
-
-    async getDataFromServer() {
-        // first get all tasks from server
     }
 
-    async addTask() {
-        // add new task to the tasks list in the server
+    async getDataFromServer() {
+        const tasks = await fetch('/loadTasks');
+        this.tasks = await tasks.json();
+    }
+
+    // add new task to the tasks list in the server and to temporary storing 
+    async addTask(inputValue) {
+        const result = await fetch('/addTask', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ title: inputValue })
+        });
+        const newTask = await result.json();
+        this.tasks.push(newTask);
+        return;
+    }
+
+    // delete task from the server and to temporary storing 
+    async deleteTask(taskId) {
+        const result = await fetch(`/${taskId}`, {
+            method: 'DELETE',
+        });
+
+        const taskToDelete = this.tasks.find(task => task.id == taskId);
+        const taskIndex = this.tasks.indexOf(taskToDelete);
+        this.tasks.splice(taskIndex, 1);
+        await result.json();
+        return;
     }
 
 }
